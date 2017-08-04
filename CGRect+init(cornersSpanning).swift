287eb9677e7905a3da70896131175140816598a0
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-04 13:52:48 +0300

    add new CGRect extension

---
+++ CGRect+init(cornersSpanning).swift
@@ -0,0 +1,21 @@
+//
+//  CGRect+init(cornersSpanning).swift
+//  DragLeft
+//
+//  Created by Wesley Van der Klomp on 8/4/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGRect {
+    
+    /// Creates a rectangle with corners reaching from `pointA` to `pointB`.
+    init(cornersSpanning from: CGPoint, to: CGPoint) {
+        let width =  to.x - from.x
+        let height =  to.y - from.y
+        
+        self.init(x: from.x, y: from.y, width: width, height: height)
+    }
+    
+}
