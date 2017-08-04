16fa2a88f8d7d23f0745b9c126a76fc403de12d4
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-04 13:54:41 +0300

    Remove copywrite

--- CGRect+init(cornersSpanning).swift
+++ CGRect+init(cornersSpanning).swift
@@ -3,7 +3,6 @@
 //  https://github.com/wvdk/A-Swift-Vocabulary
 //
 //  Created by Wesley Van der Klomp on 8/4/17.
-//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
 //
 
 import CoreGraphics
