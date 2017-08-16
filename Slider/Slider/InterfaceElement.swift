96167d100d6263b5ab219703d7942756f905193b
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-16 17:01:22 +0300

    create InterfaceElement

---
+++ Slider/Slider/InterfaceElement.swift
@@ -0,0 +1,13 @@
+//
+//  InterfaceElement.swift
+//  Slider
+//
+//  Created by Wesley Van der Klomp on 8/16/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+
+protocol InterfaceElement {
+    
+}
