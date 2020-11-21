a8bfee5b38e74ed289484bc5d939b285bbbab95a
Wes van der Klomp <wes.vdk@icloud.com>
2020-11-20 16:37:41 -0800

    Update Collection+Extensions.swift

--- Collection+Extensions.swift
+++ Collection+Extensions.swift
@@ -5,6 +5,8 @@ extension Collection {
     
     /// Returns the element at the specified index iff it is within bounds, otherwise nil.
     ///
+    /// This method is convenient but much slower than a typical subscript get. I don't recommend using it on large arrays.
+    ///
     /// Usage: `myArray[safe: 123]`
     subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
